68e75454d5b7dc8aaded4fd01fa8355a07e1b1a1
Lauren Pehlivanian <lpehlivanian00@stuy.edu>
2019-04-08 10:02:18 -0400

    added main test cases, toString() passes

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -51,8 +51,8 @@ public class ExpressionTree {
  /* The sample tree would be: "(3 + (2 * 10))"     */
  public String toString(){
    /*you are to write this method*/
-   if (isValue()) return getValue();
-   return "(" + getLeft().toString() + " "+getOp()+" " + getRight.toString() + ")";
+   if (isValue()) return ""+ getValue();
+   return "(" + getLeft().toString() + " "+getOp()+" " + getRight().toString() + ")";
  }
 
  /*return the expression as a postfix notation string without parenthesis*/
@@ -89,39 +89,40 @@ public class ExpressionTree {
    }
 
    //ugly main sorry!
+   public static void main(String[] args) {
   ExpressionTree a = new ExpressionTree(4.0);
   ExpressionTree b = new ExpressionTree(2.0);
 
   ExpressionTree c = new ExpressionTree('+',a,b);
   System.out.println(c);
-  System.out.println(c.toStringPostfix());
-  System.out.println(c.toStringPrefix());
-  System.out.println(c.evaluate());//6.0
+  //System.out.println(c.toStringPostfix());
+  //System.out.println(c.toStringPrefix());
+  //System.out.println(c.evaluate());//6.0
 
 
   ExpressionTree d = new ExpressionTree('*',c,new ExpressionTree(3.5));
   System.out.println(d);
-  System.out.println(d.toStringPostfix());
-  System.out.println(d.toStringPrefix());
-  System.out.println(d.evaluate());//21
+  //System.out.println(d.toStringPostfix());
+  //System.out.println(d.toStringPrefix());
+  //System.out.println(d.evaluate());//21
 
   ExpressionTree ex = new ExpressionTree('-',d,new ExpressionTree(1.0));
   System.out.println(ex);
-  System.out.println(ex.toStringPostfix());
-  System.out.println(ex.toStringPrefix());
-  System.out.println(ex.evaluate());//20
+//  System.out.println(ex.toStringPostfix());
+//  System.out.println(ex.toStringPrefix());
+//  System.out.println(ex.evaluate());//20
 
   ex = new ExpressionTree('+',new ExpressionTree(1.0),ex);
   System.out.println(ex);
-  System.out.println(ex.toStringPostfix());
-  System.out.println(ex.toStringPrefix());
-  System.out.println(ex.evaluate());//21
+//  System.out.println(ex.toStringPostfix());
+//  System.out.println(ex.toStringPrefix());
+//  System.out.println(ex.evaluate());//21
 
   ex = new ExpressionTree('/',ex,new ExpressionTree(2.0));
   System.out.println(ex);
-  System.out.println(ex.toStringPostfix());
-  System.out.println(ex.toStringPrefix());
-  System.out.println(ex.evaluate());//10.5
+//  System.out.println(ex.toStringPostfix());
+//  System.out.println(ex.toStringPrefix());
+//  System.out.println(ex.evaluate());//10.5
 }
 
 
